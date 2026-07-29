8b5a792151ea88ec07e8e49d4982977509874303
mattsm18 <matthew.smith181104@gmail.com>
2026-07-29 22:53:04 +1200

    renamed SerialManager

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ Purpose:
 #include <StepperMotor.h>                // StepperMotor Internal Library
 #include <PIDController.h>               // PI Controller Internal Libaray
 
-#include "Communication/CommsManager.h"  // Serial Comms Implementation
+#include "communication/SerialManager.h"  // Serial Comms Implementation
 #include "pins.h"
 
 //*** Definitions ***//
